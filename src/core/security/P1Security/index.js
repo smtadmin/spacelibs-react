79b2726d5ac9eb5a4d268b7ae8d3ffa74c43eb96
Tyler Gaffaney <tyler.gaffaney@siliconmtn.com>
2021-02-17 17:08:22 -0700

    feat: 🎸 Created Security Context and Provider

---
+++ src/core/security/P1Security/index.js
@@ -0,0 +1,17 @@
+/*
+ * File: /src/core/security/P1Security/index.js
+ * Version: 0.0.2
+ * Project: @siliconmtn/spacelibs-react
+ * Description: Index files are used to make exporting easier
+ * File Created: Wednesday, 17th February 2021 1:50 pm
+ * Author: tyler Gaffaney (tyler.gaffaney@siliconmtn.com)
+ * -----
+ * Last Modified: Wednesday, 17th February 2021 4:09 pm
+ * Modified By: tyler Gaffaney (tyler.gaffaney@siliconmtn.com>)
+ * -----
+ * Copyright 2021, Silicon Mountain Technologies, Inc.
+ */
+
+import P1Security from './P1Security';
+
+export default P1Security;
