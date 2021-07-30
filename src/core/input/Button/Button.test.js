2095cebbd954200b0fbea09e41f5a1d3b93232c8
Chris Scarola <chris.scarola@siliconmtn.com>
2021-07-30 16:39:27 -0600

    refactor(tests): update tests for data-testid changes

--- src/core/input/Button/Button.test.js
+++ src/core/input/Button/Button.test.js
@@ -6,8 +6,8 @@
  * File Created: Thursday, 11th February 2021 12:37 pm
  * Author: tyler Gaffaney (tyler.gaffaney@siliconmtn.com)
  * -----
- * Last Modified: Monday, 10th May 2021 11:14 am
- * Modified By: tyler Gaffaney (tyler.gaffaney@siliconmtn.com>)
+ * Last Modified: Friday, 30th July 2021 4:36 pm
+ * Modified By: Chris Scarola (chris.scarola@siliconmtn.com)
  * -----
  * Copyright 2021, Silicon Mountain Technologies, Inc.
  */
@@ -21,40 +21,42 @@ import userEvent from '@testing-library/user-event';
 /**
  * Checks that the button renders and the current snapshot matches the previous one.
  */
-it("Renders with no props", ()=> {
-	const element = render(<Button />);
-	expect(element).toBeTruthy();
+it('Renders with no props', () => {
+  const element = render(<Button />);
+  expect(element).toBeTruthy();
 
-	expect(element).toMatchSnapshot();
+  expect(element).toMatchSnapshot();
 });
 
 /**
  * Checks that onClick is called when the button is clicked
  */
-it("Calls onClick function when clicked", () => {
-	const mockedFunction = jest.fn();
+it('Calls onClick function when clicked', () => {
+  const mockedFunction = jest.fn();
 
-	const element = render(<Button onClick={mockedFunction}/>);
-	const inputParent = screen.getByTestId('button');
-	
-	userEvent.click(inputParent);
+  const element = render(<Button onClick={mockedFunction} />);
+  const inputParent = screen.getByTestId('generic-button');
 
-	expect(element).toBeTruthy();
-	expect(mockedFunction).toHaveBeenCalled();
+  userEvent.click(inputParent);
+
+  expect(element).toBeTruthy();
+  expect(mockedFunction).toHaveBeenCalled();
 });
 
 /**
  * Checks that when data is passed to a button, that it displays it as text
  */
-it("Renders with text", ()=> {
-	render(<Button>Something</Button>);
-	const inputParent = screen.getByTestId('button');
-	expect(inputParent.textContent).toBe("Something");
+it('Renders with text', () => {
+  render(<Button>Something</Button>);
+  const inputParent = screen.getByTestId('generic-button');
+  expect(inputParent.textContent).toBe('Something');
 });
 
-it("Renders with props", () => {
-    const element = render(<Button className={"btn"} startIcon={<div/>} endIcon={<div/>}/>);
-    expect(element).toBeTruthy();
+it('Renders with props', () => {
+  const element = render(
+    <Button className={'btn'} startIcon={<div />} endIcon={<div />} />
+  );
+  expect(element).toBeTruthy();
 
-    expect(element).toMatchSnapshot();
+  expect(element).toMatchSnapshot();
 });
