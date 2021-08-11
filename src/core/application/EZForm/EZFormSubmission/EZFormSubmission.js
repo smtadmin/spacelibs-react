6037df36a491f1641f05a3f24282d7f7108a96c0
Chris Scarola <chris.scarola@siliconmtn.com>
2021-08-11 13:53:05 -0600

    feat(cst-383): add marketing slug to submission page

--- src/core/application/EZForm/EZFormSubmission/EZFormSubmission.js
+++ src/core/application/EZForm/EZFormSubmission/EZFormSubmission.js
@@ -2,20 +2,38 @@
  * File: /src/core/application/EZForm/EZFormSubmission/EZFormSubmission.js
  * Version: 0.0.19
  * Project: @siliconmtn/spacelibs-react
- * Description: Component to display the submission screen 
+ * Description: Component to display the submission screen
  * File Created: Wednesday, 28th April 2021 9:27 am
  * Author: tyler Gaffaney (tyler.gaffaney@siliconmtn.com)
  * -----
- * Last Modified: Thursday, June 3, 2021 5:42pm
- * Modified By: James Camire (james@siliconmtn.com>)
+ * Last Modified: Wednesday, 11th August 2021 1:39 pm
+ * Modified By: Chris Scarola (chris.scarola@siliconmtn.com)
  * -----
  * Copyright 2021, Silicon Mountain Technologies, Inc.
  */
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import SMTButton from "../../../input/Button";
-import CheckCircle from "@material-ui/icons/CheckCircle";
+import SMTButton from '../../../input/Button';
+import CheckCircle from '@material-ui/icons/CheckCircle';
+
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+import { useTheme } from '@material-ui/core/styles';
+
+const newFormPrompt = (palette) => (
+  <>
+    <div
+      css={css`
+        color: ${palette.text.secondary};
+        margin-top: 20px;
+      `}>
+      Interested in making your own secure forms with EZ Form? Get started here:
+    </div>
+    <Link to="/">EZ Form Homepage</Link>
+  </>
+);
 
 /**
  * EZFormSubmission component
@@ -23,37 +41,39 @@ import CheckCircle from "@material-ui/icons/CheckCircle";
  * @param {*} {text, resubmit, formId} - Component props
  * @returns {*} React element
  */
-function EZFormSubmission({text, resubmit, uriPath}){
-	return (
-        <div className='submission-text text-center'>
-            <span style={{ fontSize: "60px" }}>
-                <CheckCircle fontSize='inherit' htmlColor={"#4fad52"} />
-            </span>
-            <h1>Thanks!</h1>
-            <h1>You&apos;re all set.</h1>
-            <h3>{text}</h3>
-            {resubmit && (
-                <SMTButton
-                    className={"resubmit-button"}
-                    color={"secondary"}
-                    onClick={() => {
-                        window.location = "/form/" + uriPath;
-                    }}>
-                    Submit Another Response
-                </SMTButton>
-            )}
-        </div>
-    );
+function EZFormSubmission({ text, resubmit, uriPath }) {
+  const palette = useTheme().palette;
+  return (
+    <div className="submission-text text-center">
+      <span style={{ fontSize: '60px' }}>
+        <CheckCircle fontSize="inherit" htmlColor={'#4fad52'} />
+      </span>
+      <h1>Thanks!</h1>
+      <h1>You&apos;re all set.</h1>
+      <h3>{text}</h3>
+      {resubmit && (
+        <SMTButton
+          className={'resubmit-button'}
+          color={'secondary'}
+          onClick={() => {
+            window.location = '/form/' + uriPath;
+          }}>
+          Submit Another Response
+        </SMTButton>
+      )}
+      {newFormPrompt(palette)}
+    </div>
+  );
 }
 
 EZFormSubmission.defaultProps = {
-    text: "The form has successfully been submitted."
+  text: 'The form has successfully been submitted.'
 };
 
 EZFormSubmission.propTypes = {
-	text: PropTypes.string,
-	resubmit: PropTypes.bool.isRequired,
-	uriPath: PropTypes.string
+  text: PropTypes.string,
+  resubmit: PropTypes.bool.isRequired,
+  uriPath: PropTypes.string
 };
 
 export default EZFormSubmission;
