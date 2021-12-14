046f83b44ce28fc992b4754bd54539c785bb2b33
Daniel-Fong <daniel.fong.mv@gmail.com>
2021-12-14 12:45:04 -0800

    fix: 🐛 max length and PII text on radio and check as well

--- src/core/application/EZForm/RadioBlock/RadioBlock.js
+++ src/core/application/EZForm/RadioBlock/RadioBlock.js
@@ -6,8 +6,8 @@
  * File Created: Friday, 19th February 2021 10:25 am
  * Author: tyler Gaffaney (tyler.gaffaney@siliconmtn.com)
  * -----
- * Last Modified: Tuesday, 10th August 2021 4:53 pm
- * Modified By: tyler Gaffaney (tyler.gaffaney@siliconmtn.com>)
+ * Last Modified: Tuesday, 14th December 2021 12:39 pm
+ * Modified By: Daniel Fong (daniel.fong@siliconmtn.com>)
  * -----
  * Copyright 2021, Silicon Mountain Technologies, Inc.
  */
@@ -114,7 +114,7 @@ class RadioBlock extends React.Component {
    */
   render() {
     return (
-      <div className="question-input-wrapper">
+      <div className='question-input-wrapper'>
         <FormControl fullWidth>
           <RadioGroup
             {...this.props}
@@ -125,7 +125,8 @@ class RadioBlock extends React.Component {
         {this.state.showAlternateResponse && (
           <TextField
             class={'select-alt-field'}
-            label={'Other'}
+            label={'Other (NO PII AUTHORIZED)'}
+            maxLength={100}
             value={this.state.alternateValue}
             onValueChanged={this.onAlternateValueChanged.bind(this)}
           />
